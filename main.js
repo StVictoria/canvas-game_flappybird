6b4a12fb53d987d81ef013af3e8265a92c20834d
StVictoria <2018vvs@gmail.com>
2020-11-26 11:03:58 +0300

    score

--- main.js
+++ main.js
@@ -10,11 +10,21 @@ let frame = 0;
 let score = 0;
 let gamespeed = 2;
 
+const gradient = ctx.createLinearGradient(0, 0, 0, 65);
+gradient.addColorStop("0.2", "#fff");
+gradient.addColorStop("0.5", "#000");
+gradient.addColorStop("0.55", "#4040ff");
+gradient.addColorStop("0.6", "#000");
+gradient.addColorStop("0.9", "#fff");
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   handleObstacles();
   bird.update();
   bird.draw();
+  ctx.fillStyle = gradient;
+  ctx.font = "60px Georgia";
+  ctx.fillText(score, 10, 50);
   handleParticles();
   if (handleCollision()) return;
   requestAnimationFrame(animate);
@@ -44,6 +54,13 @@ function handleCollision() {
           bird.y + bird.height < canvas.height))
     ) {
       ctx.drawImage(bang, bird.x - 40, bird.y - 35, 100, 70);
+      ctx.font = "25px Georgia";
+      ctx.fillStyle = "black";
+      ctx.fillText(
+        "Game over, your score is " + score,
+        160,
+        canvas.height / 2 - 12
+      );
       return true;
     }
   }
